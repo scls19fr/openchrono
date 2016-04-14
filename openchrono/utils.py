91b5fbfa32bac00de77864116b54eedcd95207e6
scls19fr <s.celles@gmail.com>
2016-04-14 22:25:37 +0200

    Add a mapv function similar to Arduino map function

--- openchrono/utils.py
+++ openchrono/utils.py
@@ -3,6 +3,12 @@
 
 from __future__ import print_function, absolute_import
 
+
+def mapv(x, x0, x1, y0, y1, typ=None):
+    if typ is None:
+        typ = type(x)
+    return typ((y1 - y0) / (x1 - x0) * (x - x0) + y0)
+
 def linear_function_with_limit(x, xmin, xmax, ymin, ymax):
     y = (ymax - ymin) / (xmax - xmin) * (x - xmin) + ymin
     if y < ymin:
